4c6b2db7642652e8612eaeb1fbedec73273de5d8
Tulasiram Paruchuri <tulasiram.paruchuri@planonsoftware.com>
2019-03-11 17:56:00 +0530

    demo

---
+++ Catalog/Controllers/CatalogController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    [Route("api/catalogs")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet,Route("")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "usd", "inr","jpy" };
+        }
+    }
+}
---
+++ Orders/Controllers/OrdersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Orders.Controllers
+{
+    [Route("api/orders")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet,Route("")]
+        public ActionResult<IEnumerable<Order>> Get()
+        {
+            return new Order[] { new Order{Id=1,Amount=500},new Order{Id=2,Amount=700} };
+        }
+    }
+
+    public class Order
+{
+    public long Id {get;set;}
+    public double Amount {get;set;}
+
+}
+}
